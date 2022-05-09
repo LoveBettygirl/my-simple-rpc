6409ee50f8d358e521c87237a60add0296f672a4
LoveBettygirl <bettycyygirl@163.com>
2022-05-09 13:43:25 +0800

    Update rpcapplication

--- src/rpcapplication.cpp
+++ src/rpcapplication.cpp
@@ -1,7 +1,37 @@
 #include "rpcapplication.h"
+#include <iostream>
+#include <unistd.h>
+#include <string>
+
+void ShowArgsHelp()
+{
+    std::cout << "format: command -i <configfile>" << std::endl;
+}
 
 void RpcApplication::Init(int argc, char *argv[])
 {
+    if (argc < 2) {
+        ShowArgsHelp();
+        exit(EXIT_FAILURE);
+    }
+
+    int c = 0;
+    std::string config_file;
+    while ((c = getopt(argc, argv, "i:")) != -1) {
+        switch (c) {
+            case 'i':
+                config_file = optarg;
+                break;
+            case '?':
+            case ':':
+                ShowArgsHelp();
+                exit(EXIT_FAILURE);
+            default:
+                break;
+        }
+    }
+
+    // 开始加载配置文件了
 
 }
 
